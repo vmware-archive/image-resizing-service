62e064762e1422feadd508541818201a9b9c75ef
Michael Goddard <mgoddard@gopivotal.com>
2016-10-15 12:34:53 -0400

    Turning DEBUG off

--- image_resizer.py
+++ image_resizer.py
@@ -60,4 +60,4 @@ def thumb():
     return send_file(img_io, mimetype='image/jpeg')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=port, threaded=True, debug=False)
